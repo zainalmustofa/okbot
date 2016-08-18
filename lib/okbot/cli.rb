236042d8075fb6dd5059d8c22e55c21645038de0
zainal mustofa <zenald92@gmail.com>
2016-08-18 12:27:28 +0700

    update gem

--- lib/okbot/cli.rb
+++ lib/okbot/cli.rb
@@ -1,17 +1,19 @@
 require 'rubygems'
 require 'thor'
 require 'nokogiri'
-require 'rainbow'
+require 'rainbow/ext/string'
 require 'okbot/cli/stack'
 require 'net/http'
 require 'json'
 require "highline/import"
 require 'terminal-table/import'
 require 'pry'
+require 'open-uri'
 
 module Okbot
   class MyCli < Thor
     desc "blitz", "Get schedule theater on blitz bandung"
+
     def blitz
       puts "Please choice your theater!"
       puts "1. pvj"
@@ -21,14 +23,18 @@ module Okbot
       puts "3. bec"
       puts "----------"
       answer = STDIN.gets.chomp
-
+      print "Doing your schedule..."
+      spin_it{
+        sleep rand(4)+2
+      }
+      print "done!"
       case answer 
       when "1", "pvj"
         doc = Nokogiri::XML(open("https://www.cgvblitz.com/en/schedule/cinema/0100"))
         puts Rainbow("Result schedule PVJ MALL").yellow
         puts "--------------------"
         doc.css('.schedule-title').children.each do |l|
-          puts Rainbow(l.content).aqua
+          puts l.content.color(:cyan)
         end
         puts "--------------------"
       when "2", "miko"
@@ -36,7 +42,7 @@ module Okbot
         puts Rainbow("Result schedule MIKO MALL").yellow
         puts "--------------------"
         doc.css('.schedule-title').children.each do |l|
-          puts Rainbow(l.content).aqua
+          puts l.content.color(:cyan)
         end
         puts "--------------------"
       when "3", "bec"
@@ -44,7 +50,7 @@ module Okbot
         puts Rainbow("Result schedule BEC MALL").yellow
         puts "--------------------"
         doc.css('.schedule-title').children.each do |l|
-          puts Rainbow(l.content).aqua
+          puts l.content.color(:cyan)
         end
         puts "--------------------"
       else
@@ -52,6 +58,23 @@ module Okbot
       end
     end
 
+    desc "spin_it", "FOR LOADING"
+    def spin_it(fps=10)
+      chars = %w[⣾ ⣽ ⣻ ⢿ ⡿ ⣟ ⣯ ⣷]
+      delay = 1.0/fps
+      iter = 0
+      spinner = Thread.new do
+        while iter do  # Keep spinning until told otherwise
+          print chars[(iter+=1) % chars.length]
+          sleep delay
+          print "\b"
+        end
+      end
+      yield.tap{       # After yielding to the block, save the return value
+        iter = false   # Tell the thread to exit, cleaning up after itself…
+        spinner.join   # …and wait for it to do so.
+      }                # Use the block's return value as the method's
+    end
     desc "stack COMMANDS", "Get Stack overflow question"
     subcommand "stack", Okbot::CLI::Stack
   end
--- lib/okbot/cli/stack.rb
+++ lib/okbot/cli/stack.rb
@@ -1,10 +1,15 @@
 module Okbot
   module CLI
     class Stack < Thor
-      desc "stack title", "Search stack overflow question and get link"
+      desc "title", "Search stack overflow question and get link"
       option :tags
       def title
         title = ask "Input text: "
+        print "Doing your title..."
+        spin_it{
+          sleep rand(4)+2
+        }
+        
         url = "https://api.stackexchange.com/2.2/search/advanced?pagesize=5&order=desc&sort=activity&title=#{title}&site=stackoverflow&filter=!1zSk2G.tfS9G_M7ErPqm("
         uri = URI(url)
         response  = Net::HTTP.get(uri)
@@ -20,6 +25,24 @@ module Okbot
         end
         puts table([nil, Rainbow("Result for: #{title}").red], *rows)
       end
+
+    desc "spin_it", "FOR LOADING"
+    def spin_it(fps=10)
+      chars = %w[⣾ ⣽ ⣻ ⢿ ⡿ ⣟ ⣯ ⣷]
+      delay = 1.0/fps
+      iter = 0
+      spinner = Thread.new do
+        while iter do  # Keep spinning until told otherwise
+          print chars[(iter+=1) % chars.length]
+          sleep delay
+          print "\b"
+        end
+      end
+      yield.tap{       # After yielding to the block, save the return value
+        iter = false   # Tell the thread to exit, cleaning up after itself…
+        spinner.join   # …and wait for it to do so.
+      }                # Use the block's return value as the method's
+    end
     end
   end
 end
